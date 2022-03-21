418d22bbcff92f01b723b124b52a77c6d5d08a90
alismith <alimsmith19@gmail.com>
2022-03-21 15:53:45 -0600

    model

---
+++ Models/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTemple.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int AppointmentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Size { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        public string AppointmentTime { get; }
+    }
+}
